1deb5d992db78a370520919630cc6b3a54eb693a
vverq <18631869w@gmail.com>
2022-11-26 18:37:14 +0500

    refactoring

--- hw1.11/main.py
+++ hw1.11/main.py
@@ -5,42 +5,50 @@ import re
 
 
 class Strategy(ABC):
-    def __init__(self, url):
-        html = request.urlopen(url).read().decode("utf8")
-        parser = bs4.BeautifulSoup(html, "html.parser")
-        self.title = parser.find("title").text
-
     @abstractmethod
-    def parse(self) -> str:
+    def parse(self, html: str) -> str:
         pass
 
 
 class Parser:
     strategy: Strategy
 
-    def __init__(self, strategy: Strategy) -> None:
+    def __init__(self, strategy: Strategy, url: str) -> None:
         self.strategy = strategy
+        self.html = request.urlopen(url).read().decode("utf8")
 
     def get_title(self) -> str:
-        return self.strategy.parse()
+        return self.strategy.parse(self.html)
 
 
 class StrategyYouTube(Strategy):
-    def parse(self) -> str:
-        title = re.search("(.+) - YouTube", self.title)
-        return title.group(1) if title else ""
+    def parse(self, html: str) -> str:
+        parser = bs4.BeautifulSoup(html, "html.parser")
+        tag = parser.find("title")
+        if tag:
+            title = re.search("(.+) - YouTube", tag.text)
+            return title.group(1) if title else ""
+        return ""
 
 
 class StrategyYandexMusic(Strategy):
-    def parse(self) -> str:
-        title = re.search("(.+) слушать онлайн на Яндекс Музыке", self.title)
-        return title.group(1) if title else ""
+    def parse(self, html: str) -> str:
+        parser = bs4.BeautifulSoup(html, "html.parser")
+        tag = parser.find("title")
+        if tag:
+            title = re.search("(.+) слушать онлайн", tag.text)
+            return title.group(1) if title else ""
+        return ""
 
 
 class StrategyHabr(Strategy):
-    def parse(self) -> str:
-        title = re.search("(.+) / Хабр", self.title)
-        return title.group(1) if title else ""
+    def parse(self, html: str) -> str:
+        parser = bs4.BeautifulSoup(html, "html.parser")
+        tag = parser.find("title")
+        if tag:
+            title = re.search("(.+) / Хабр", tag.text)
+            return title.group(1) if title else ""
+        return ""
 
 
 if __name__ == "__main__":
@@ -48,9 +56,9 @@ if __name__ == "__main__":
     youtube_url = "https://www.youtube.com/watch?v=90RLzVUuXe4"
     habr_url = "https://habr.com/ru/post/280238/"
 
-    youtube_parser = Parser(StrategyYouTube(youtube_url))
-    yandex_music_parser = Parser(StrategyYandexMusic(yandex_url))
-    habr_parser = Parser(StrategyHabr(habr_url))
+    youtube_parser = Parser(StrategyYouTube(), youtube_url)
+    yandex_music_parser = Parser(StrategyYandexMusic(), yandex_url)
+    habr_parser = Parser(StrategyHabr(), habr_url)
 
     print(youtube_parser.get_title())
     print(yandex_music_parser.get_title())
