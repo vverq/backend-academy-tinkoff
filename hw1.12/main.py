87d303c98d92a29bb49f59d7c2eb238fc6d7bea0
vverq <18631869w@gmail.com>
2022-12-17 22:13:05 +0500

    правки после ревью

--- hw1.12/main.py
+++ hw1.12/main.py
@@ -1,13 +1,16 @@
 import asyncio
+import multiprocessing
 import pathlib
 from aiohttp import web
 from concurrent.futures import ThreadPoolExecutor
 from yaml import safe_load
+import requests
 
 
 class Daemon:
     def __init__(self, name: str):
-        with open(f"configurations/conf_{name}.yml", "r") as file:
+        path = pathlib.Path(f"configurations/conf_{name}.yml")
+        with path.open("r") as file:
             data = safe_load(file)
             self.port = data["port"]
             self.directory = data["directory"]
@@ -15,9 +18,16 @@ class Daemon:
             self.save_file_from_other = data["save_file_from_other"]
             self.app = web.Application()
             self.app.add_routes([web.get("/{name}", self.get_file_content)])
+            self.app.add_routes([web.get("/other_nodes_files/{name}", self.get_file_content_by_other_node)])
 
     def run(self):
         web.run_app(self.app, host="localhost", port=self.port)
+
+    async def get_file_content_by_other_node(self, request: web.Request):
+        filename = str(request.match_info.get("name"))
+        file_content = await read_file(self.directory, filename)
+        if file_content:
+            return web.Response(text=str(file_content))
 
     async def get_file_content(self, request: web.Request):
         filename = str(request.match_info.get("name"))
@@ -25,15 +35,11 @@ class Daemon:
         if file_content:
             return web.Response(text=str(file_content))
         else:
-            tasks = []
             for node in self.nodes:
-                node_daemon = Daemon(node)
-                function = read_file(node_daemon.directory, filename)
-                task = asyncio.create_task(function)
-                tasks.append(task)
-            results = list(filter(lambda x: x, await asyncio.gather(*tasks)))
-            if results:
-                file_content = str(results[0])
+                file_content = requests.get(f'{node["host"]}:{node["port"]}/other_nodes_files/{filename}')
+                if file_content:
+                    break
+            if file_content:
                 if self.save_file_from_other:
                     await write_file(self.directory, filename, file_content)
                 return web.Response(text=file_content)
@@ -59,7 +65,16 @@ async def write_file(directory: str, filename: str, content: str):
 
 def main():
     a = Daemon("a")
-    a.run()
+    b = Daemon("b")
+    c = Daemon("c")
+
+    process_a = multiprocessing.Process(target=a.run)
+    process_b = multiprocessing.Process(target=b.run)
+    process_c = multiprocessing.Process(target=c.run)
+
+    process_a.start()
+    process_b.start()
+    process_c.start()
 
 
 if __name__ == "__main__":
